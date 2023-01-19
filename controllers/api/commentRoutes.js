ea67174e5dc3e14cb555740499cec5d7d7b24445
DiegoM <medeldiego47@yahoo.com>
2023-01-18 20:51:15 -0600

    find by id comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -6,3 +6,10 @@ router.get('/',(req,res)=>{
     Comment.findAll({}).then(data=>res.json(data))
 })
 
+router.get('/:id',(req,res)=>{
+    Comment.findOne({
+        where: {
+            id:req.params.id
+        }
+    }).then(data => res.json(data))
+})
